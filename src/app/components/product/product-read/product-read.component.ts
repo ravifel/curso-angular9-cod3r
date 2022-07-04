6cf69692a5534c083c880cadb7321a920177edc3
Ravi Silva <ravifelipee@gmail.com>
2022-07-04 01:54:29 -0300

    atualizando a tabela com os produtos

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -10,17 +10,18 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit {
 
   products: Product[]
+  displayedColumns = ["id", "name", "price"]
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-     this.productService.read()
-     .subscribe(
-      products => {
-        this.products = products
-        console.log(products)
-      }
-     )
+    this.productService.read()
+      .subscribe(
+        products => {
+          this.products = products
+          console.log(products)
+        }
+      )
   }
 
 }
